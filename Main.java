7f3505eba2ec98476e0e04a7e0d3104e7de481a0
subo-9439 <kws3363@gmail.com>
2021-10-31 14:40:10 +0900

    main

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
-public class Main{
-    StringConcat str = (s,v) -> System.out.println(s+v);
-    str.makeString("a","b");
-}
+import java.util.Scanner;
 
-interface StringConcat {
-    public void makeString(String s1, String s2);
-}ㅁㅇㄴsadas
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static Scanner sc = new Scanner(System.in);
+    static int N,M;
+    static int[] selected;
+
+    static void input(){
+        N = Integer.parseInt(sc.next());
+        M = Integer.parseInt(sc.next());
+    }
+
+    static void rec_func(int k){
+        if(k == M+1){
+            for(int i=0; i<=M;i++) sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        }else{
+            for(int cand = 1; cand<=N; cand++){
+                selected[k] = cand;
+                rec_func(k+1);
+                selected[k] = 0;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        input();
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+}
