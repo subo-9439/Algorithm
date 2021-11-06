8048b8a8a79cc1b649b9a071d93745821a7fcddb
subo-9439 <kws3363@gmail.com>
2021-11-06 15:46:05 +0900

    백퀸

--- Main.java
+++ Main.java
@@ -1,20 +1,78 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
     static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
-
-    static int n;
-    static ArrayList<Integer>[] adj;
-    static int[] parent;
-
+    static int N,cnt;
+    static int col[];
+    
     static void input(){
-        n = sc.nextInt();
-        adj = new ArrayList[n+1];
-        parent = new int[n+1];
-        for(int i=1; i<=n; i++){
-            adj[i] = new ArrayList<>();
+        N = sc.nextInt();
+        col = new int[N+1];
+    }
+    static boolean attackable(int r1, int c1,int r2, int c2){
+        if(c1 == c2) return true;
+        if(r1-c1 == r2-c2) return true;
+        if(r1+c1 == r2+c2) return true;
+        return false;
+    }
+
+    static void rec_func(int row){
+        if(row == N+1) cnt++;
+        else{
+            for(int c = 1; c <= N; c++){
+                boolean possible = true;
+                for(int r = 1; r<= row-1; r++){
+                    if(attackable(row, c, r, col[r])){
+                        possible = false;
+                        break;
+                    }
+                }
+                if(possible){
+                    col[row] = c;
+                    rec_func(row+1);
+                    col[row] = 0;
+                }
+            }
         }
+    }
+    public static void main(String[] args) {
+        input();
+        rec_func(1);
+        System.out.println(cnt);
+    }
+    static class FastReader{
+        BufferedReader br ;
+        StringTokenizer st ;
         
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while(st==null|| !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
