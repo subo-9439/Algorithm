e6947f11f71e12032525f9833cd596d43f89a0d0
subo-9439 <kws3363@gmail.com>
2021-11-10 19:00:07 +0900

    복습

--- Main.java
+++ Main.java
@@ -1,59 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
     static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static int N,cnt;
-    static int col[];
-    
+
+    static int N, M, start;
+    static int[][] adj;
+    static boolean[] check;
+
     static void input(){
         N = sc.nextInt();
-        col = new int[N+1];
-    }
-    static boolean attackable(int r1, int c1,int r2, int c2){
-        if(c1 == c2) return true;
-        if(r1-c1 == r2-c2) return true;
-        if(r1+c1 == r2+c2) return true;
-        return false;
-    }
+        M = sc.nextInt();
+        start = sc.nextInt();
+        adj = new int[N+1][N+1];
+        check = new boolean[N+1];
 
-    static void rec_func(int row){
-        if(row == N+1) cnt++;
-        else{
-            for(int c = 1; c <= N; c++){
-                boolean possible = true;
-                for(int r = 1; r<= row-1; r++){
-                    if(attackable(row, c, r, col[r])){
-                        possible = false;
-                        break;
-                    }
-                }
-                if(possible){
-                    col[row] = c;
-                    rec_func(row+1);
-                    col[row] = 0;
-                }
+        for(int i=0;i<M;i++){
+            int x = sc.nextInt(), y= sc.nextInt();
+            adj[x][y] = 1;
+            adj[y][x] = 1;
+        }
+    }
+    static void dfs(int x){
+        check[x] = true;
+        sb.append(x).append(' ');
+        for(int i = 1; i <= N; i++){
+            if(adj[x][i] == 0)continue;
+            if(check[i])continue;
+            dfs(i);
+        }
+    }  
+    static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        check[start] = true;
+        
+        while(!q.isEmpty()){
+            int x = q.poll();
+
+            sb.append(x).append(' ');
+            for(int y=1; y<=N; y++){
+                if(check[y]) continue;
+                if(adj[x][y] == 0) continue;
+                q.add(y);
+                check[y] = true;
             }
         }
+
     }
     public static void main(String[] args) {
         input();
-        rec_func(1);
-        System.out.println(cnt);
+        dfs(start);
+        for(int i=1;i<=N;i++)check[i] = false;
+        // bfs(start);
+        sb.append('\n');
+        bfs(start);
+        System.out.println(sb);
     }
     static class FastReader{
         BufferedReader br ;
-        StringTokenizer st ;
-        
+        StringTokenizer st;
+
         public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-
+        
         String next(){
-            while(st==null|| !st.hasMoreElements()){
+            while(st == null || !st.hasMoreElements()){
                 try{
                     st = new StringTokenizer(br.readLine());
                 }catch(IOException e){
@@ -62,17 +80,10 @@ public class Main{
             }
             return st.nextToken();
         }
+
         int nextInt(){
             return Integer.parseInt(next());
         }
-        String nextLine(){
-            String str = "";
-            try{
-                str = br.readLine();
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-            return str;
-        }
+        
     }
 }
