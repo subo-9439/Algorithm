bd635284c0bc967318b219b5308f3d55d3efc375
subo-9439 <kws3363@gmail.com>
2021-11-11 23:42:26 +0900

    graph

--- Main.java
+++ Main.java
@@ -8,70 +8,56 @@ import java.util.StringTokenizer;
 public class Main{
     static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
-
-    static int N, M, start;
-    static int[][] adj;
-    static boolean[] check;
+    
+    static int N,K;
+    static int dist[];
+    static boolean visit[];
 
     static void input(){
         N = sc.nextInt();
-        M = sc.nextInt();
-        start = sc.nextInt();
-        adj = new int[N+1][N+1];
-        check = new boolean[N+1];
-
-        for(int i=0;i<M;i++){
-            int x = sc.nextInt(), y= sc.nextInt();
-            adj[x][y] = 1;
-            adj[y][x] = 1;
-        }
+        K = sc.nextInt();
+        dist = new int[100005];
+        visit = new boolean[100005];
     }
-    static void dfs(int x){
-        check[x] = true;
-        sb.append(x).append(' ');
-        for(int i = 1; i <= N; i++){
-            if(adj[x][i] == 0)continue;
-            if(check[i])continue;
-            dfs(i);
-        }
-    }  
-    static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        check[start] = true;
-        
-        while(!q.isEmpty()){
-            int x = q.poll();
-
-            sb.append(x).append(' ');
-            for(int y=1; y<=N; y++){
-                if(check[y]) continue;
-                if(adj[x][y] == 0) continue;
-                q.add(y);
-                check[y] = true;
+    static void bfs(){
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(N);
+        visit[N] = true;
+        while(!Q.isEmpty()){
+            int x = Q.poll();
+            if(x-1>=0 && !visit[x-1]){
+                dist[x-1] = dist[x]+1;
+                visit[x-1] = true;
+                Q.add(x-1);
+            }
+            if(x+1<=100000 && !visit[x+1]){
+                dist[x+1] = dist[x] +1;
+                visit[x+1] = true;
+                Q.add(x+1);
+            }
+            if(2*x <=100000 && !visit[2*x]){
+                dist[2*x] = dist[x] + 1;
+                visit[2*x] = true;
+                Q.add(2*x);
             }
         }
-
+        System.out.println(dist[K]);
     }
     public static void main(String[] args) {
         input();
-        dfs(start);
-        for(int i=1;i<=N;i++)check[i] = false;
-        // bfs(start);
-        sb.append('\n');
-        bfs(start);
-        System.out.println(sb);
+        bfs();
     }
+
     static class FastReader{
-        BufferedReader br ;
+        BufferedReader br;
         StringTokenizer st;
 
         public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-        
+
         String next(){
-            while(st == null || !st.hasMoreElements()){
+            while(st==null || !st.hasMoreElements()){
                 try{
                     st = new StringTokenizer(br.readLine());
                 }catch(IOException e){
@@ -84,6 +70,6 @@ public class Main{
         int nextInt(){
             return Integer.parseInt(next());
         }
-        
+
     }
 }
