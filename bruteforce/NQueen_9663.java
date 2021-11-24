c3fd9ec808a8b3fd99ae4fcb05d5d8c4b0949aba
subo <kws3363@gmail.com>
2021-11-24 23:28:15 +0900

    수정

--- bruteforce/NQueen_9663.java
+++ bruteforce/NQueen_9663.java
@@ -1,74 +1,95 @@
 package bruteforce;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 public class NQueen_9663 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N,M;
-    static int[] nums;
-    static int[] selected;
-
-    static void input(){
+    static void input() {
         N = scan.nextInt();
-        M = scan.nextInt();
-        nums = new int[N+1];
-        selected = new int[M+1];
-        for(int i=1; i<=N; i++){
-            nums[i]= scan.nextInt();
+        col = new int[N + 1];
+    }
+
+    static int N, ans;
+    static int[] col;
+
+    static boolean attackable(int r1, int c1, int r2, int c2) {
+        if (c1 == c2) return true;
+        if (r1 - c1 == r2 - c2) return true;
+        if (r1 + c1 == r2 + c2) return true;
+        return false;
+    }
+
+    static void rec_func(int row) {
+        if (row == N + 1) {
+            ans++;
+        } else {
+            for (int c = 1; c <= N; c++) {
+                boolean possible = true;
+                // valid check (row, c)
+                for (int i=1;i<=row-1;i++){
+                    // (i, col[i])
+                    if (attackable(row, c, i, col[i])){
+                        possible = false;
+                        break;
+                    }
+                }
+                if (possible) {
+                    col[row] = c;
+                    rec_func(row + 1);
+                    col[row] = 0;
+                }
+            }
         }
     }
-    static void rec_func(int k){
-        for(int cand=1;cand<=M;cand++){
-//            if()
-            selected[k] = cand;
-            rec_func(k+1);
-            selected[k] = 0;
-        }
-    }
+
     public static void main(String[] args) {
         input();
-        Arrays.sort(nums);
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
         rec_func(1);
+        System.out.println(ans);
     }
-    static class FastReader{
+
+
+    static class FastReader {
         BufferedReader br;
         StringTokenizer st;
-        public FastReader(){
+
+        public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        String next(){
-            while(st==null||!st.hasMoreElements()){
-                try{
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
 
-        int nextInt(){
+        int nextInt() {
             return Integer.parseInt(next());
         }
-        long nextLong(){
+
+        long nextLong() {
             return Long.parseLong(next());
         }
-        double nextDouble(){
+
+        double nextDouble() {
             return Double.parseDouble(next());
         }
 
-        String nextLine(){
-            String str="";
+        String nextLine() {
+            String str = "";
             try {
-                str= br.readLine();
-            }catch (IOException e){
+                str = br.readLine();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             return str;
