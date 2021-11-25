10516bee4842f53ff72976f4533fb04df516b330
subo <kws3363@gmail.com>
2021-11-25 13:18:25 +0900

    수정

--- bruteforce/NQueen_9663.java
+++ bruteforce/NQueen_9663.java
@@ -1,5 +1,10 @@
 package bruteforce;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class NQueen_9663 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
@@ -53,46 +58,22 @@ public class NQueen_9663 {
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
-
-        public FastReader() {
+        
+        public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
+        String next(){
+            while(st == null || !st.hasMoreElements()){
+                try{
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
+                }catch(IOException e){
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
-
-        int nextInt() {
+        int nextInt(){
             return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
         }
     }
 }
