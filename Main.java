6441f6c551be04e90f09e13dd8afb72cff58aa10
subo <kws3363@gmail.com>
2021-12-01 18:23:21 +0900

    a

--- Main.java
+++ Main.java
@@ -1,55 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main{
     static FastReader sc = new FastReader();
-    static int N, cnt;
-    static int[] col;
-
+    static StringBuilder sb = new StringBuilder();
+    static int N ,curCnt, modeCnt;
+    static long mode;
+    static long[] nums;
     static void input(){
         N = sc.nextInt();
-        col = new int[N+1];
+        nums = new long[N];
+        for(int i=0; i<N; i++){
+            nums[i] = sc.nextLong();
+        }
     }
-    static boolean attackable(int r1, int c1, int r2, int c2){
-        if(c1 == c2) return true;
-        if(r1-c1 == r2-c2) return true;
-        if(r1+c1 == r2+c2) return true;
-        return false;
-
-    }
-    static void rec_func(int row){
-        if(row == N+1){
-            cnt++;
-        }else{
-            for(int c=1; c<=N;c++){
-                boolean possible = true;
-                for(int r1 = 1;r1<=row-1; r1++){
-                    if(attackable(r1,col[r1] , row, c)){
-                        possible = false;
-                        break;
-                    }
-                }
-                if(possible){
-                    col[row] = c;
-                    rec_func(row+1);
-                    col[row] = 0;
-                }
+    static void pro(){
+        Arrays.sort(nums);
+        for(int i=0; i<N; i++){
+            if(i == 0 || nums[i] != nums[i-1]){
+                curCnt = 1;
+            }else{
+                curCnt++;
+            }
+            if(curCnt > modeCnt){
+                modeCnt = curCnt;
+                mode = nums[i];
             }
         }
+        sb.append(mode);
+        System.out.println(sb);
     }
     public static void main(String[] args) {
         input();
-        rec_func(1);
-        System.out.println(cnt);
+        pro();
     }
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
+
         public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
+
         String next(){
             while(st == null || !st.hasMoreElements()){
                 try{
@@ -60,8 +55,12 @@ public class Main{
             }
             return st.nextToken();
         }
+
         int nextInt(){
             return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
         }
     }
 }
