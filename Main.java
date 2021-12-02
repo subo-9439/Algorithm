33607c0ca3fb92de697c1ebfc46d56fcd0d6e2d8
subo <kws3363@gmail.com>
2021-12-02 15:18:19 +0900

    a

--- Main.java
+++ Main.java
@@ -1,37 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main{
     static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static int N ,curCnt, modeCnt;
-    static long mode;
-    static long[] nums;
+    static int N;
+    static String[] words;
     static void input(){
         N = sc.nextInt();
-        nums = new long[N];
-        for(int i=0; i<N; i++){
-            nums[i] = sc.nextLong();
+        words = new String[N];
+        for(int i = 0; i < N ; i++){
+            words[i] = sc.next();
         }
     }
     static void pro(){
-        Arrays.sort(nums);
-        for(int i=0; i<N; i++){
-            if(i == 0 || nums[i] != nums[i-1]){
-                curCnt = 1;
-            }else{
-                curCnt++;
-            }
-            if(curCnt > modeCnt){
-                modeCnt = curCnt;
-                mode = nums[i];
-            }
-        }
-        sb.append(mode);
-        System.out.println(sb);
+        
     }
     public static void main(String[] args) {
         input();
@@ -40,11 +25,9 @@ public class Main{
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
-
         public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-
         String next(){
             while(st == null || !st.hasMoreElements()){
                 try{
@@ -55,12 +38,8 @@ public class Main{
             }
             return st.nextToken();
         }
-
         int nextInt(){
             return Integer.parseInt(next());
-        }
-        long nextLong(){
-            return Long.parseLong(next());
         }
     }
 }
