4ec63bac670d21e1cbc62e1417f77dee754fcd60
subo <kws3363@gmail.com>
2021-12-03 21:17:19 +0900

    bp

--- Main.java
+++ Main.java
@@ -4,30 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
-    static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static int N;
-    static String[] words;
+    static FastReader sc = new FastReader();
+    static int N,M;
+    static int[] selected;
+    
     static void input(){
         N = sc.nextInt();
-        words = new String[N];
-        for(int i = 0; i < N ; i++){
-            words[i] = sc.next();
-        }
+        M = sc.nextInt();
+        selected = new int[M+1];
     }
-    static void pro(){
-        
+    static void pro(int k){
+        if(k == M+1){
+            for(int i=1; i<=M; i++)sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        }else{
+            for(int i=selected[k-1]; i<=N; i++){
+                if(i == 0) i = 1;
+                selected[k] = i;
+                pro(k+1);
+                selected[k] = 0;
+            }
+        }
     }
     public static void main(String[] args) {
         input();
-        pro();
+        pro(1);
+        System.out.println(sb);
     }
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
+
         public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
+
         String next(){
             while(st == null || !st.hasMoreElements()){
                 try{
@@ -40,6 +52,9 @@ public class Main{
         }
         int nextInt(){
             return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
         }
     }
 }
