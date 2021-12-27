134129a558a4086d44c71417a271530f7975dcd4
subo <kws3363@gmail.com>
2021-12-27 21:35:26 +0900

    tree

--- Main.java
+++ Main.java
@@ -1,58 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+//Boj_1068 트리
 public class Main{
     static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static int n;
-    static int[][] arr;
-    static int[][] dp;
+
+    static int n, root, erased;
+    static ArrayList<Integer>[] child;
+    static int[] leaf;
 
     static void input(){
         n = sc.nextInt();
-        arr = new int[n+1][n+1];
-        dp = new int[n+1][n+1];
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                arr[i][j] = sc.nextInt();
+        child = new ArrayList[n];
+        leaf = new int[n];
+        for(int i=0;i<n;i++) child[i] = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int par = sc.nextInt();
+            //root 노드 찾기
+            if(par == -1){
+                root = i;
+                continue;
             }
+            child[par].add(i);
+        }
+        erased = sc.nextInt();
+    }
+    static void dfs(int x){
+        if(child[x].isEmpty()){
+            leaf[x] = 1;
+        }
+        for (int y: child[x]){
+            dfs(y);
+            leaf[x] += leaf[y];
         }
     }
-        static void pro(){
-        //초기값
-        dp[1][1] = arr[1][1];
-
-        //두번씩
-        for(int i = 2; i <= n; i++){
-            for(int j = 1; j < i; j++){
-                for(int cnt = 0; cnt<2; cnt++){
-                    if(arr[i][j+cnt]+dp[i-1][j] > dp[i][j+cnt])
-                    dp[i][j+cnt] = arr[i][j+cnt]+dp[i-1][j];
-
-                }
+    static void pro(){  
+        for(int i=0;i<n;i++){
+            if(child[i].contains(erased)){
+                child[i].remove(child[i].indexOf(erased));
             }
         }
-        int ans = 0;
-        for(int i = 1; i <= n; i++){
-            if(dp[n][i] > ans) ans = dp[n][i];
-        }   
-        sb.append(ans);
+        if (root != erased) dfs(root);
+        System.out.println(leaf[root]);
     }
     public static void main(String[] args) {
         input();
         pro();
-        System.out.println(sb);
     }
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
+
         public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
+
         String next(){
-            while(st==null || !st.hasMoreElements()){
+            while(st == null || !st.hasMoreElements()){
                 try{
                     st = new StringTokenizer(br.readLine());
                 }catch(IOException e){
@@ -60,7 +68,8 @@ public class Main{
                 }
             }
             return st.nextToken();
-        }        
+        }
+
         int nextInt(){
             return Integer.parseInt(next());
         }
