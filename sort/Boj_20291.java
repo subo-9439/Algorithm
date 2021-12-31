51ab7518b1d42a974589520fcc8a813d7114c6b2
subo <kws3363@gmail.com>
2022-01-01 01:02:15 +0900

    sort

--- sort/Boj_20291.java
+++ sort/Boj_20291.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj_20291 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
