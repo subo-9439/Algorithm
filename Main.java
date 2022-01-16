8189b129e3c752bf31624b3dea48b39e3d913932
subo-9439 <kws3363@gmail.com>
2022-01-16 18:11:03 +0900

    2022-01-16

--- Main.java
+++ Main.java
@@ -1,80 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
     
     static int N,M;
-    static int[] cards, finds;
+    static int[] A;
     static void input(){
         N = scan.nextInt();
-        cards = new int[N+1];
-        for(int i=1; i<=N; i++){
-            cards[i] = scan.nextInt();
+        A = new int[N+1];
+        for(int i = 1; i <= N; i++){
+            A[i] = scan.nextInt();
         }
-        //
         M = scan.nextInt();
-        finds = new int[M];
-        for(int i=0; i<M ;i++){
-            finds[i] = scan.nextInt();
-        }
-
-        
     }
-    static int lower_bound(int[] arr, int L, int R, int X){
-        int ans = R + 1;
-        while(L <= R){
-            int mid = (L+R)/2;
-            if(arr[mid] >= X) {
-                R = mid - 1;
-                ans = mid;
-            }else{
-                L = mid + 1;
-            }
-        }
-        return ans;
-    }
-    static int upper_bound(int[] arr, int L, int R, int X){
-        int ans = R + 1;
-        while(L <= R) {
-            int mid = (L+R)/2;
-            if(arr[mid] > X){
-                R = mid - 1;
-                ans = mid;
-            }else {
-                L = mid + 1;
-            }
+    static boolean determinant(long X){
+        int sum = 0;
+        for(int i = 1; i <= N; i++){
+            sum += A[i]-X;
 
         }
-        return ans;
+        return sum > M;
     }
     static void pro(){
-        Arrays.sort(cards,1,N+1);
-        for(int find:finds){
-            int upper = upper_bound(cards, 1, N, find);
-            int lower = lower_bound(cards, 1, N, find);
-            sb.append(upper-lower).append(' ');
+        long L = 0, R = 1000000000, ans = 0;
+        while(L <= R){
+            long mid = (L+R)/ 2;
+            if(determinant(mid)){
+                L = mid + 1;
+                ans = mid;
+            }else{
+                R = mid - 1;
             }
-        System.out.println(sb);
+        }
+        System.out.println(ans);
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         input();
         pro();
     }
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
-
         public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        String next(){
-            while (st == null || !st.hasMoreElements()){
+        String next() {
+            while(st == null || !st.hasMoreElements()){
                 try{
                     st = new StringTokenizer(br.readLine());
                 }catch(IOException e){
@@ -83,7 +59,7 @@ public class Main {
             }
             return st.nextToken();
         }
-        
+
         int nextInt() {
             return Integer.parseInt(next());
         }
