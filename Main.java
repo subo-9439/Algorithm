f33834eb923d2c5a29e4aa33709e2a13ddf2bdc4
subo-9439 <kws3363@gmail.com>
2022-01-21 01:42:10 +0900

    2022-01-21

--- Main.java
+++ Main.java
@@ -1,82 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
+class State{
+    int[] X;
+    State(int[] X){
+        this.X = new int[3];
+        for(int i=0; i<3; i++){
+            this.X[i] = X[i];
+        }
+    }
+
+    State move(int from, int to, int[] Limit){
+
+        int[] nX = new int[] {X[1],X[2],X[3]};
+        if(nX[from] + nX[to] <= Limit[to]){
+            nX[to] = nX[from] + nX[to];
+            nX[from] = 0;
+        }else{
+            nX[to] = Limit[to];
+            nX[from] -= Limit[to] -nX[to]; 
+        }
+
+        return new State(nX);
+    }
+}
 public class Main{
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static int N,M,V;
-    static int[][] arr;
-    static boolean[] visit;
+
+    static int[] Limit;
+    static boolean[] possible;
+    static boolean[][][] visit;
 
     static void input(){
-        N = scan.nextInt();
-        M = scan.nextInt();
-        V = scan.nextInt();
-        arr = new int[N+1][N+1];
-        visit = new boolean[N+1];
-        for(int i=1; i<=M; i++){
-            int x = scan.nextInt(), y = scan.nextInt();
-            arr[x][y] = 1;
-            arr[y][x] = 1;
+        Limit = new int[3];
+        for(int i=0; i<3; i++){
+            Limit[i] = scan.nextInt();
         }
+        possible = new boolean[205];
+        visit = new boolean[205][205][205];
     }
-
-    static void dfs(int x) {
-        // x 를 방문했다.
-        visit[x] = true;
-        sb.append(x).append(' ');
-
-        // x 에서 갈 수 있는 곳들을 작은 번호부터 모두 방문한다.
-        for (int y = 1; y <= N; y++) {
-            if (adj[x][y] == 0) continue;
-
-            // y 를 이미 갈 수 있다는 사실을 안다면, 굳이 갈 필요 없다.
-            if (visit[y])
-                continue;
-
-            // y에서 갈 수 있는 곳들도 확인 해보자
-            dfs(y);
-        }
-    }
-
-    // start 에서 시작해서 갈 수 있는 정점들을 모두 탐색하기
-    static void bfs(int start) {
-        Queue<Integer> que = new LinkedList<>();
-
-        // start는 방문 가능한 점이므로 que에 넣어준다.
-        que.add(start);
-        visit[start] = true;  // start를 갈 수 있다고 표시하기 (중요!!!)
-
-        while (!que.isEmpty()) {  // 더 확인할 점이 없다면 정지
-            int x = que.poll();
-
-            sb.append(x).append(' ');
-            for (int y = 1; y <= N; y++) {
-                if (adj[x][y] == 0) continue;
-                if (visit[y]) continue;  // x 에서 y 를 갈 수는 있지만, 이미 탐색한 점이면 무시
-                
-                // y를 갈 수 있으니까 que에 추가하고, visit 처리 하기!
-                que.add(y);
-                visit[y] = true;
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        dfs(V);
-        sb.append('\n');
-        for(int i = 1; i <= N; i++) visit[i] = false;
-        bfs(V);
-        System.out.println(sb);
+        
     }
-
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
+
         public FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
@@ -95,5 +67,6 @@ public class Main{
         int nextInt(){
             return Integer.parseInt(next());
         }
+        
     }
 }
