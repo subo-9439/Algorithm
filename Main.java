957fc35977b3d96d0585fa55b0141fae09c0646f
subo-9439 <kws3363@gmail.com>
2022-02-03 23:52:07 +0900

    2022-02-03

--- Main.java
+++ Main.java
@@ -1,43 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static int N, budget;
-    static int[] arr;
-    static int ans;
+    static int N, M;
+    static int[][] arr;
+    static boolean[] visit;
 
     static void input(){
         N = scan.nextInt();
-        arr = new int[N+1];
-        for(int i=1; i<=N; i++){
-            arr[i] = scan.nextInt();
-        }
-        budget = scan.nextInt();
+        M = scan.nextInt();
+        arr = new int[N+1][N+1];
+        visit = new boolean[N+1];
+        for(int i = 1; i <= M ; i++){
+            int x = scan.nextInt(), y= scan.nextInt();
+            arr[x][y] = 1;
+            arr[y][x] = 1;
+        }        
     }
-
-    static boolean determinant(int k){
-        int sum = 0;
-        for(int i=1 ; i<=N; i++){
-            sum += Math.min(arr[i], k);
-        }
-        return sum <= budget;
-    }
-    static void pro(){
-        int L = 0, R = 1000000000;
-        while(L<=R){
-            int mid = (L+R)/2;
-            if(determinant(mid)){
-                ans = mid;
-                L = mid + 1;
-            }else{
-                R = mid - 1;
+    static void bfs(int x){
+        int cnt = 1;
+        ArrayList<Integer> a = new ArrayList<>();
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(x);
+        visit[x] = true;
+        while(!Q.isEmpty()){
+            x = Q.poll();
+            System.out.println(x);
+            for(int y=1; y<=N; y++){
+                if(visit[y]) continue;
+                if(arr[x][y] == 0)continue;
+                visit[y] = true;
+                System.out.println(y);
+                cnt++;
             }
         }
-        System.out.println(ans);
+        a.add(cnt);
+        System.out.println(cnt);
+    }
+    static void pro(){
+        for(int i=1; i<=N; i++){
+            if(visit[i]) continue;
+            bfs(i);
+        }
+        
     }
     public static void main(String[] args) {
         input();
@@ -52,7 +64,7 @@ public class Main{
         }
 
         String next(){
-            while(st==null || !st.hasMoreElements()){
+            while(st == null || !st.hasMoreElements()){
                 try{
                     st = new StringTokenizer(br.readLine());
                 }catch(IOException e){
