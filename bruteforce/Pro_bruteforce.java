7e7223d13b812849bb802f6983e80635670241fb
subo-9439 <kws3363@gmail.com>
2022-02-15 22:33:10 +0900

    2022-02-15 : bruteforce 1

--- bruteforce/Pro_bruteforce.java
+++ bruteforce/Pro_bruteforce.java
@@ -1,9 +1,19 @@
-class Solution {
+package bruteforce;
+
+//프로그래머스 Lv2
+//브르투포스 
+public class Pro_bruteforce {
+  class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-
+        int multiple = 0;
+        //가로 2x 세로 2y  = 10+4
+        //x <=7
+        //y <= 7-x
+        
         for(int i = 1; i <= brown/2 + 2; i++){
             for(int j = 1; j <= brown/2 - i + 2; j++){
+
                 if(i*j == (brown+yellow)) {
                     answer[0] = i; answer[1] = j;
                 }
@@ -11,4 +21,5 @@ class Solution {
         }
         return answer;
     }
+}
 }
--- dijkstra/Boj_1916.java
+++ dijkstra/Boj_1916.java
@@ -1,8 +1,8 @@
+package dijkstra;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
