2aeda132ca7416e9de9ff72b556742d13c1bd00d
subo-9439 <kws3363@gmail.com>
2022-03-03 20:20:29 +0900

    커밋

--- Solution.java
+++ Solution.java
@@ -1,37 +1,6 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-
 class Solution {
-    public ArrayList<String> solution(String[] record) {
-        HashMap<String,String> map = new HashMap<>();
-        ArrayList<String> answer = new ArrayList<>();
-
-        for(int i = 0; i < record.length; i++){
-            StringTokenizer st = new StringTokenizer(record[i]);
-
-            String behavior = st.nextToken();
-            String id = st.nextToken();
-            if(behavior.equals("Leave")) continue;
-            String name = st.nextToken();
-            map.put(id, name);
-        }
-
-        for(int i = 0; i< record.length;i++){
-            StringTokenizer st = new StringTokenizer(record[i]);
-            String behavior = st.nextToken();
-            String id = st.nextToken();
-            if(behavior.equals("Enter")){
-                answer.add(map.get(id)+"님이 들어왔습니다.");
-            }
-            if(behavior.equals("Leave")){
-                answer.add(map.get(id)+"님이 나갔습니다.");
-            }
-        }
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
         return answer;
- 
     }
-
-
-
 }
