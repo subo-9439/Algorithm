33e51c764efb88096855bf009289847a004c2a3b
subo-9439 <kws3363@gmail.com>
2022-03-14 00:12:22 +0900

    카카오 거리두기 확인하기

--- Solution.java
+++ Solution.java
@@ -1,28 +1,118 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        StringBuilder sb = new StringBuilder();
+    static Queue<Node> queue = new LinkedList<>();
+    static ArrayList<Node> list;
+    static int[][] dir = {{1,0},{0,1},
+    {-1,0},{0,-1},
+    {1,1},{-1,-1},
+    {1,-1},{-1,1},
+    {0,2},{2,0},
+    {-2,0},{0,-2}
+    };  //12개 2개
 
-        for(int i = 0; i < n ; i++){
-            String temp = Integer.toBinaryString(arr1[i]|arr2[i]);
-            System.out.println(temp);  
-
-
-            //길이가 넘어가는거 처리
-            int length = n;
-            while(length-- > temp.length()){
-                sb.append(' ');
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        
+        char[][] map = new char[5][5];
+        
+        
+        //i = 테스트케이스 개수
+        for(int t = 0; t < 5; t++){
+            //테스트케이스 시작
+            list = new ArrayList<>();
+            for(int i = 0; i < 5; i++){
+                for(int j = 0; j < 5; j++){
+                    map[i][j] = places[t][i].charAt(j);
+                    if(places[t][i].charAt(j) == 'P'){
+                        Node n = new Node(i, j);
+                        list.add(n);
+                        queue.add(n);
+                    }
+                }
             }
+            //조건ㅊ자기
+            boolean distance = false;
+            System.out.println("test"+t);
+            for(int i = 0 ; i<list.size();i++){
 
-            //
-            for(int j = 0 ; j < temp.length(); j++){
-                if(temp.charAt(j) == '1') sb.append('#');
-                else sb.append(' ');
+                Node pos = list.get(i);
+                int x = pos.x;
+                int y = pos.y;
+                
+                for(int j = 0; j <list.size(); j++){
+                    if( i== j) continue;
+                    Node another = list.get(j);
+                    int nx = another.x;
+                    int ny = another.y;
+                    //2이상이면 거리두기 성공
+                    if(Math.abs(nx-x)+Math.abs(ny-y) > 2) continue;
+                    //아니면 조건 찾아봐야함
+                    else{
+                        System.out.println("x,y:"+x+", "+y+"nx,ny:"+nx+", "+ny);
+                       //가로 세로 1일때
+                       if(nx == x+1 && ny == y) distance = true;
+                       else if(nx == x-1 && ny == y) distance =true;
+                       else if(nx == x && ny == y-1) distance = true;
+                       else if(nx == x && ny == y+1) distance = true;
+                       
+                       //대각선
+                       if(nx == x+1 && ny == y+1) {
+                           if(map[x+1][y] =='X' && map[x][y+1]== 'X') distance= false;
+                           else   distance = true;
+                       }
+                       else if(nx == x-1 && ny== y+1) {
+                            if(map[x-1][y] =='X' && map[x][y+1]== 'X') distance= false;
+                            else   distance = true;
+                       }
+                       else if(nx == x-1 && ny== y-1) {
+                            if(map[x-1][y] =='X' && map[x][y-1]== 'X') distance= false;
+                            else   distance = true;
+                       }
+                       else if(nx == x+1 && ny == y-1) {
+                            if(map[x+1][y] =='X' && map[x][y-1]== 'X') distance= false;
+                             else   distance = true;
+                       }
+
+                       //두개 가로 세로 2
+                       if(nx == x+2 && ny == y) {
+                           if(map[x+1][y] == 'X') distance = false;
+                           else distance =true;
+                       }
+                       else if(nx == x-2 && ny == y) {
+                            if(map[x-1][y] == 'X') distance = false;
+                            else distance =true;
+                       }
+                       else if(ny == x && ny == y-2) {
+                            if(map[x][y-1] == 'X') distance = false;
+                            else distance =true;
+                       }
+                       else if(nx == x && ny == y+2) {
+                            if(map[x][y+1] == 'X') distance = false;
+                            else distance =true;
+                       }
+
+
+                    }
+                    if(distance) break;
+                }
+                if(distance) break;
+
             }
-            answer[i] = sb.toString();
-            sb.setLength(0);
+            if(distance) answer[t] = 0;
+            else answer[t] = 1;
         }
-
         return answer;
+    }
+
+    class Node{
+        int x;
+        int y;
+        Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
---
+++ kakao/Pro_거리두기확인하기.java
@@ -0,0 +1,112 @@
+package kakao;
+  import java.util.ArrayList;
+public class Pro_거리두기확인하기 {
+
+
+  static ArrayList<Node> list;
+  class Solution {
+
+      public int[] solution(String[][] places) {
+          int[] answer = new int[5];
+          
+          char[][] map = new char[5][5];
+          
+          
+          //i = 테스트케이스 개수
+          for(int t = 0; t < 5; t++){
+              //테스트케이스 시작
+              list = new ArrayList<>();
+              for(int i = 0; i < 5; i++){
+                  for(int j = 0; j < 5; j++){
+                      map[i][j] = places[t][i].charAt(j);
+                      if(places[t][i].charAt(j) == 'P'){
+                          Node n = new Node(i, j);
+                          list.add(n);
+                      }
+                  }
+              }
+              //조건ㅊ자기
+              boolean distance = false;
+              System.out.println("test"+t);
+              for(int i = 0 ; i<list.size();i++){
+
+                  Node pos = list.get(i);
+                  int x = pos.x;
+                  int y = pos.y;
+                  
+                  for(int j = 0; j <list.size(); j++){
+                      if( i== j) continue;
+                      Node another = list.get(j);
+                      int nx = another.x;
+                      int ny = another.y;
+                      //2이상이면 거리두기 성공
+                      if(Math.abs(nx-x)+Math.abs(ny-y) > 2) continue;
+                      //아니면 조건 찾아봐야함
+                      else{
+                          System.out.println("x,y:"+x+", "+y+"nx,ny:"+nx+", "+ny);
+                        //가로 세로 1일때
+                        if(nx == x+1 && ny == y) distance = true;
+                        else if(nx == x-1 && ny == y) distance =true;
+                        else if(nx == x && ny == y-1) distance = true;
+                        else if(nx == x && ny == y+1) distance = true;
+                        
+                        //대각선
+                        if(nx == x+1 && ny == y+1) {
+                            if(map[x+1][y] =='X' && map[x][y+1]== 'X') distance= false;
+                            else   distance = true;
+                        }
+                        else if(nx == x-1 && ny== y+1) {
+                              if(map[x-1][y] =='X' && map[x][y+1]== 'X') distance= false;
+                              else   distance = true;
+                        }
+                        else if(nx == x-1 && ny== y-1) {
+                              if(map[x-1][y] =='X' && map[x][y-1]== 'X') distance= false;
+                              else   distance = true;
+                        }
+                        else if(nx == x+1 && ny == y-1) {
+                              if(map[x+1][y] =='X' && map[x][y-1]== 'X') distance= false;
+                              else   distance = true;
+                        }
+
+                        //두개 가로 세로 2
+                        if(nx == x+2 && ny == y) {
+                            if(map[x+1][y] == 'X') distance = false;
+                            else distance =true;
+                        }
+                        else if(nx == x-2 && ny == y) {
+                              if(map[x-1][y] == 'X') distance = false;
+                              else distance =true;
+                        }
+                        else if(ny == x && ny == y-2) {
+                              if(map[x][y-1] == 'X') distance = false;
+                              else distance =true;
+                        }
+                        else if(nx == x && ny == y+2) {
+                              if(map[x][y+1] == 'X') distance = false;
+                              else distance =true;
+                        }
+
+
+                      }
+                      if(distance) break;
+                  }
+                  if(distance) break;
+
+              }
+              if(distance) answer[t] = 0;
+              else answer[t] = 1;
+          }
+          return answer;
+      }
+
+  
+  }
+  static class Node{
+    int x;
+    int y;
+    Node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+}
