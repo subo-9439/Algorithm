2b5fbc859851ad8a65e858979326bb6600bd3270
subo-9439 <kws3363@gmail.com>
2022-04-01 22:36:45 +0900

    다단계 칫솔판매

--- Solution.java
+++ Solution.java
@@ -1,55 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = new int[queries.length];
-        int[][] map = new int[rows+1][columns+1];;
-        int[][] temp = new int[rows+1][columns+1];
 
-        int num = 1;
-        for(int i = 1; i<rows+1; i++){
-            for(int j = 1; j<columns+1; j++){
-                map[i][j] = num;
-                temp[i][j] = num++;
-            }
-        }
-        int drawCount = 0,idx=0;
-        for(int i = 0; i < queries.length; i++){
-            ArrayList<Integer> list = new ArrayList<>();
-            int x1 = queries[i][0];
-            int y1 = queries[i][1];
-            int x2 = queries[i][2];
-            int y2 = queries[i][3];
-            drawCount = x2 - x1;
-            for(int k = 1; k <=drawCount; k++){
-                temp[x2-k][y1] = map[x2+-k+1][y1];
-                list.add(temp[x2-k][y1]);
-            }
-            drawCount = y2 - y1;
-            for(int k = 1; k <= drawCount; k++){
-                temp[x1][y1+k] = map[x1][y1+k-1];
-                list.add(temp[x1][y1+k]);
-            }
-            drawCount = x2 - x1;
-            for(int k = 1; k <=drawCount; k++){
-                temp[x1+k][y2] = map[x1+k-1][y2];
-                list.add(temp[x1+k][y2]);
-
-            }
-            drawCount = y2 - y1;
-            for(int k = 1; k <= drawCount; k++){
-                temp[x2][y2-k] = map[x2][y2-k+1];
-                list.add(temp[x2][y2-k]);
-
-            }
-            //깊은 복사
-            for(int j = 0; j < map.length; j++){
-                System.arraycopy(temp[j],0,map[j],0,temp[0].length);
-            }
-            answer[idx++]=Collections.min(list);
-        }
-        return answer;
-    }
 }
---
+++ 날짜별/a220329/Pro_다단계칫솔판매.java
@@ -0,0 +1,39 @@
+package 날짜별.a220329;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pro_다단계칫솔판매 {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+        //enroll 이름  referral 추천인 seller 판매원 amount 판애원이 판량 * 100
+
+        Map<String, Integer> cashMap = new HashMap<>();
+        Map<String, String> nameMap = new HashMap<>();
+        //초기화
+        for(int i = 0 ; i < enroll.length; i++){
+            nameMap.put(enroll[i],referral[i]);
+            cashMap.put(enroll[i],0);
+        }
+        for(int i = 0; i< seller.length; i++){
+            String name = seller[i];
+            int curCash = amount[i]*100;
+            cashMap.put(name,cashMap.get(name)+curCash);
+            while (nameMap.containsKey(name)){
+                cashMap.put(name, cashMap.get(name) - (int) (curCash*0.1) );
+                String next = nameMap.get(name);
+                if(nameMap.containsKey(next)){
+                    cashMap.put(next, cashMap.get(next) + (int) (curCash*0.1));
+                }
+                curCash *=0.1;
+                if(curCash == 0) break;;
+                name = next;
+            }
+        }
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = cashMap.get(enroll[i]);
+        }
+
+        return answer;
+    }
+}
