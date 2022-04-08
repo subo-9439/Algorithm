52785b26702f6224dd7a3e3ef6f6ea69672801b3
subo-9439 <kws3363@gmail.com>
2022-04-08 21:20:08 +0900

    백트래킹 문제1

--- Main.java
+++ Main.java
@@ -3,50 +3,79 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 	static FastReader scan = new FastReader();
-	static char[] str1,str2;
-	static Integer[][] dp;
+	static int[][] board;
+	static boolean[] visited;
+	static int N;
+	static int Min = Integer.MAX_VALUE;
 	static void input(){
-		str1 = scan.nextLine().toCharArray();
-		str2 = scan.nextLine().toCharArray();
-		dp = new Integer[str1.length][str2.length];
-	}
-
-	static int LCS(int x, int y){
-		//인덱스 밖(공집합)일 경우 0 반환
-		if(x == -1 || y == -1) {
-			return 0;
-		}
-		// 만약 탐색하지 않은 인덱스라면?
-		if(dp[x][y] == null) {
-			dp[x][y] = 0;
-			// str1의 x번째 문자와 str2의 y번째 문자가 같은지 검사
-			if(str1[x] == str2[y]) {
-				dp[x][y] = LCS(x-1,y-1)+1;
-			}
-			//같지 않다면 LCS(dp)[x-1][y]와 LCS(dp)[x,y-1] 중 값으로 초기화
-			else{
-				dp[x][y] = Math.max(LCS(x-1,y),LCS(x,y-1));
+		N = scan.nextInt();
+		visited = new boolean[N];
+		board = new int[N][N];
+		for(int i = 0 ; i < N; i++){
+			for(int j = 0; j < N; j++){
+				board[i][j] = scan.nextInt();
 			}
 		}
-		return dp[x][y];
 	}
+	static void comb(int idx, int count){
+		if(count == N / 2){
+			calc();
+			return;
+		}
+		for(int i = idx; i < N; i++){
+			if(!visited[i]) {
+				visited[i] = true;
+				comb(i+1 , count + 1);
+				visited[i] = false;
+			}
+		}
+	}
+	static void calc(){
+		int team_start = 0;
+		int team_link = 0;
 
+		for (int i = 0; i < N-1; i++){
+			for(int j = i + 1; j < N; j++){
+				//i번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수
+				if(visited[i] && visited[j]) {
+					team_start += board[i][j];
+					team_start += board[j][i];
+				}
+				//i번째 사람과 j번째 사람이 false라면 링크팀으로 점수
+				else if(!visited[i] && !visited[j]){
+					team_link += board[i][j];
+					team_link += board[j][i];
+				}
+			}
+		}
+		int val = Math.abs(team_start-team_link);
+		//더이상 찾을 필요가없으므로 시스템을 종료한다
+		if(val == 0){
+			System.out.println(val);
+			System.exit(0);
+		}
+		Min = Math.min(val,Min);
+	}
 	public static void main(String[] args) {
 		input();
-		System.out.println(LCS(str1.length-1,str2.length-1));
+		comb(0,0);
+		System.out.println(Min);
+
 	}
 	static class FastReader{
 		BufferedReader br;
 		StringTokenizer st;
+
 		public FastReader(){
 			br = new BufferedReader(new InputStreamReader(System.in));
 		}
+
 		String next(){
-			while (st==null || !st.hasMoreTokens()){
+			while (st == null|| !st.hasMoreTokens()){
 				try{
-					st=new StringTokenizer(br.readLine());
+					st = new StringTokenizer(br.readLine());
 				}catch (IOException e){
 					e.printStackTrace();
 				}
@@ -55,15 +84,6 @@ public class Main {
 		}
 		int nextInt(){
 			return Integer.parseInt(next());
-		}
-		String nextLine(){
-			String str = "";
-			try{
-				str = br.readLine();
-			}catch (IOException e){
-				e.printStackTrace();
-			}
-			return str;
 		}
 	}
 }
---
+++ 날짜별/a220329/Boj_스타트와링크.java
@@ -0,0 +1,93 @@
+package 날짜별.a220329;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/*
+Boj_14889 조합접근
+*/
+public class Boj_스타트와링크 {
+    static FastReader scan = new FastReader();
+    static int[][] board;
+    static boolean[] visited;
+    static int N;
+    static int Min = Integer.MAX_VALUE;
+    static void input(){
+        N = scan.nextInt();
+        visited = new boolean[N];
+        board = new int[N][N];
+        for(int i = 0 ; i < N; i++){
+            for(int j = 0; j < N; j++){
+                board[i][j] = scan.nextInt();
+            }
+        }
+    }
+    static void comb(int idx, int count){
+        if(count == N / 2){
+            calc();
+            return;
+        }
+        for(int i = idx; i < N; i++){
+            if(!visited[i]) {
+                visited[i] = true;
+                comb(i+1 , count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+    static void calc(){
+        int team_start = 0;
+        int team_link = 0;
+
+        for (int i = 0; i < N-1; i++){
+            for(int j = i + 1; j < N; j++){
+                //i번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수
+                if(visited[i] && visited[j]) {
+                    team_start += board[i][j];
+                    team_start += board[j][i];
+                }
+                //i번째 사람과 j번째 사람이 false라면 링크팀으로 점수
+                else if(!visited[i] && !visited[j]){
+                    team_link += board[i][j];
+                    team_link += board[j][i];
+                }
+            }
+        }
+        int val = Math.abs(team_start-team_link);
+        //더이상 찾을 필요가없으므로 시스템을 종료한다
+        if(val == 0){
+            System.out.println(val);
+            System.exit(0);
+        }
+        Min = Math.min(val,Min);
+    }
+    public static void main(String[] args) {
+        input();
+        comb(0,0);
+        System.out.println(Min);
+
+    }
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while (st == null|| !st.hasMoreTokens()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+}
