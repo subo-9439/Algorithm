1798424e2110f3735804a06029559f819ea5396c
subo <kws3363@gmail.com>
2022-04-10 22:44:22 +0900

    백트래킹 1문제

--- al_22_04/Boj_치킨배달.java
+++ al_22_04/Boj_치킨배달.java
@@ -1,10 +1,13 @@
+package al_22_04;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
-public class Main{
+//Boj 15685
+//
+public class Boj_치킨배달 {
 	static FastReader scan = new FastReader();
 	static int N,M;
 	static int[][] map;
@@ -37,23 +40,27 @@ public class Main{
 		dfs(0,0);
 	}
 	static void dfs(int start, int cnt){
+    //M만큼 선택된다면
 		if(cnt == M){
 			int res = 0;
-
+      //각 집에서 
 			for(int i = 0; i < home.size(); i++) {
 				int temp = Integer.MAX_VALUE;
+        // M만큼 선택된 치킨집까지 거리를 계산한다.
+        // ex) M = 2라면 각집에서 두집까지의 최소거리 = res
 				for(int j = 0; j < chickens.size(); j++){
 					if(visited[j]) {
-						int dist = Math.abs(home.get(i).x - chickens.get(j).x)
-						+ Math.abs(home.get(i).y - chickens.get(j).y);
+						int dist = Math.abs(home.get(i).x - chickens.get(j).x)+ Math.abs(home.get(i).y - chickens.get(j).y);
 						temp = Math.min(temp, dist);
 					}
 				}
 				res += temp;
 			}
+      // 여러 치킨집을 돌면서 두집거리중 가장 작은 ans를 구함
 			ans = Math.min(ans, res);
 			return;
 		}
+    //치킨집을 개수 만큼 선택한다.
 		for(int i = start; i< chickens.size(); i++){
 			visited[i] = true;
 			dfs(i + 1, cnt+ 1);
