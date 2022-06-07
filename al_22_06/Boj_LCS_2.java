240686b0fbe7d383bc740985a2c767c90fb51c39
subo-9439 <kws3363@gmail.com>
2022-06-07 22:12:55 +0900

    LCS 2문제

--- al_22_06/Boj_LCS_2.java
+++ al_22_06/Boj_LCS_2.java
@@ -3,18 +3,21 @@ package al_22_06;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-//9251
+//9252
 /*
  * dp(i-1 j) , dp(i , j-1) 중에 가장 큰값을 넣어준다.
  * 또는 i== j 일 때는 dp[i][j] = dp[i-1][j-1] + 1 을 해준다.
  * */
-public class Boj_LCS {
+public class Boj_LCS_2 {
     static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
     static char[] chaArr1;
     static char[] chaArr2;
     static int[][] dp;
+    static char[] s;
     static void input(){
         chaArr1 = scan.next().toCharArray();
         chaArr2 = scan.next().toCharArray();
@@ -32,12 +35,41 @@ public class Boj_LCS {
                 max = Math.max(max,dp[i+1][j+1]);
             }
         }
+        String a = String.valueOf(chaArr1);
+        String b = String.valueOf(chaArr2);
 
-        System.out.println(max);
+        sb.append(max).append("\n");
+        getLcsToString(a,a.length(),b.length());
+
+        System.out.println(sb.toString());
+
+    }
+
+    //거꾸로 생각하면 편함
+    static void getLcsToString(String str,int str1Len, int str2Len){
+        Stack<Character> st = new Stack<>();
+        while (str1Len > 0 && str2Len > 0) {
+//            if(str1Len == 0 || str2Len  == 0 )break;
+
+            if (dp[str1Len][str2Len] == dp[str1Len-1][str2Len]){
+                str1Len--;
+            }else if(dp[str1Len][str2Len] == dp[str1Len][str2Len-1]){
+                str2Len--;
+            }else {
+                st.push(str.charAt(str1Len-1));
+                str1Len--;
+                str2Len--;
+            }
+        }
+        while (!st.isEmpty()){
+            sb.append(st.pop());
+        }
+
     }
     public static void main(String[] args) {
         input();
         pro();
+
     }
     static class FastReader{
         BufferedReader br;
